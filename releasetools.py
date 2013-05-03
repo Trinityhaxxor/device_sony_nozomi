5c69a2a62bfabd435f2e5b257054541c55ed3cc9
RaymanFX <raymanfx@gmail.com>
2013-05-03 20:56:41 +0200

    releasetools-extensions: drop useless code
    Change-Id: I00e31b34b95057f08ee45bc1d557964d90cfad51

--- releasetools.py
+++ releasetools.py
@@ -19,39 +19,9 @@ import common
 import os
 import shutil
 
-OPTIONS = common.OPTIONS
-OPTIONS.sizehack = True
-
 TARGET_DIR = os.getenv('OUT')
 
-def ReplaceLine(self):
-    edify = self.script
-    for i in xrange(len(edify.script)):
-        if 'mount("ext4", "EMMC", "/dev/block/mmcblk0p14", "/data");' in edify.script[i]:
-            edify.script[i] = 'run_program("/sbin/busybox", "mount", "/data");'
-
-        elif 'unmount("/data");' in edify.script[i]:
-            edify.script[i] = 'run_program("/sbin/busybox", "umount", "/data");'
-
-        elif 'mount("ext4", "EMMC", "/dev/block/mmcblk0p12", "/system");' in edify.script[i]:
-            edify.script[i] = 'run_program("/sbin/busybox", "mount", "/system");'
-
-        elif 'unmount("/system");' in edify.script[i]:
-            edify.script[i] = 'run_program("/sbin/busybox", "umount", "/system");'
-
-        elif 'ui_print("Formatting system...");' in edify.script[i]:
-            edify.script[i] += '\nrun_program("/sbin/busybox", "mount", "/system");\ndelete_recursive("/system");'
-
-        elif 'format("ext4", "EMMC", "/dev/block/mmcblk0p12", "0");' in edify.script[i]:
-            edify.script[i] = ''
-
-def WriteBootImage(self):
-    edify = self.script
-    edify.script.append(
-        'package_extract_file("boot.img", "/dev/block/mmcblk0p3");' )
-
 def FullOTA_Assertions(self):
-       ReplaceLine(self)
 
        #Copy boot.img verbatim
        bootimage_path = os.path.join(TARGET_DIR, "boot.elf")
@@ -60,12 +30,12 @@ def FullOTA_Assertions(self):
        os.mkdir(prebuilt_dir)
        shutil.copyfile(bootimage_path, prebuilt_path)
 
-       #Force writing of boot.img to /boot
-       WriteBootImage(self)
-
 def IncrementalOTA_Assertions(self):
-       ReplaceLine(self)
 
-       #Force writing of boot.img to /boot
-       WriteBootImage(self)
+       #Copy boot.img verbatim
+       bootimage_path = os.path.join(TARGET_DIR, "boot.elf")
+       prebuilt_dir = os.path.join(self.input_tmp, "BOOTABLE_IMAGES")
+       prebuilt_path = os.path.join(prebuilt_dir, "boot.img")
+       os.mkdir(prebuilt_dir)
+       shutil.copyfile(bootimage_path, prebuilt_path)
 
